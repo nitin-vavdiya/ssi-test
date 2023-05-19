7bd160434bfae9e7a54df476c274f14f15d5c706
Nitin Vavdiya <nitin.vavdiya@smartsensesolutions.com>
2023-05-19 11:48:10 +0530

    init

---
+++ src/main/java/com/ssi/test/ssitest/SsiTestApplication.java
@@ -0,0 +1,13 @@
+package com.ssi.test.ssitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsiTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssi/test/ssitest/SsiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssi.test.ssitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
